4989c687461ce5a11bd16fe9a4107ae3642d4e64
Abdurahman Ahmed <abdahmed@live.ca>
2019-11-22 00:58:23 -0600

    lesson 20 notes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response
 from models import db, User
 import random
+import uuid
+import hashlib
 
 app = Flask(__name__)
 db.create_all()
@@ -8,28 +10,54 @@ db.create_all()
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    session_token = request.cookies.get("session_token")
+
+    if session_token:
+        user = db.query(User).filter_by(session_token=session_token).first()
+    else:
+        user = None
+    return render_template("index.html", user=user)
 
 
 @app.route("/populate", methods=["POST"])
 def populate():
     name = request.form.get("user-name")
     email = request.form.get("user-email")
+    password = request.form.get("user-password")
+    print(password)
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
     secret_number = random.randint(1, 30)
 
-    user = User(name=name, email=email, secret_number=secret_number)
+    # check if user already exists
+    user = db.query(User).filter_by(email=email).first()
 
+    if not user:
+        # create User object
+        user = User(name=name, email=email, secret_number=secret_number, password=hashed_password)
+        # add to db
+        db.add(user)
+        db.commit()
+
+    if hashed_password != user.password:
+        return "WRONG PASSWORD! Go back and try again."
+
+    session_token = str(uuid.uuid4())
+    user.session_token = session_token
     db.add(user)
     db.commit()
-    return render_template("index.html")
+
+    response = make_response(redirect(url_for("index")))
+    response.set_cookie("session_token", session_token, httponly=True, samesite='Strict')
+
+    return response
 
 
 @app.route("/guess", methods=["POST"])
 def guess():
     user_guess = int(request.form.get("guess"))
-    email = request.form.get("user-email")
+    session_token = request.cookies.get("session_token")
 
-    user = db.query(User).filter_by(email=email).first()
+    user = db.query(User).filter_by(session_token=session_token).first()
     if user:
         guess = user.secret_number
         if guess == user_guess:
